507693f66590abcae35193b829da634deb2af812
Jair Foro <jair.foro@olx.com>
2018-09-22 20:47:52 +0100

    add repository

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,10 @@
 <?php
 
-
 $router->get('/', function () use ($router) {
     return [
         "name" => "Foodness API",
         "description" => "The must fast and furious Food API",
-        "keywords" => ["food", "api", "meal"]
+        "repository" => "https://github.com/jairforo/foodness-api",
+        "keywords" => ["food", "api"]
     ];
 });
